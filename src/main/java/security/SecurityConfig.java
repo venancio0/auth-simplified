dcfba2a0e6753d64074895bf65a43cd86b49c8c4
gabriel <gabvcampaner@gmail.com>
2024-09-12 21:56:36 -0300

    security

---
+++ src/main/java/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anzMatchers("/", "/login").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .oauth2Login();
+
+        return http.build();
+    }
+}
