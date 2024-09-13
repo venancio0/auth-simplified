90343e39d6b357b06547d293e5c15370a5c9a972
gabriel <gabvcampaner@gmail.com>
2024-09-12 21:56:27 -0300

    controller de login, sessão e configs

---
+++ src/test/java/com/example/oauth2_auth_system/Oauth2AuthSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2_auth_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2AuthSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
